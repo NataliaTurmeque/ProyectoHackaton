43f25d03f7ff80e882cfc42b98c4685c3f8c0a8c
NataliaTurmeque <nataliaturmeque1018@gmail.com>
2023-06-15 12:16:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import Loguito from '../src/assets/Logoremo.png'
+
+function App() {
+
+  return (
+    <div >
+      <nav>
+<div class="logo"><img className="logo"src={Loguito}></img></div>
+<div class="barnav">
+<div>Inciar Sesion</div>
+<div>Registrar</div>
+<div>Acerca de</div>
+<div>Información</div>
+</div>
+</nav>
+<div class="contenido">
+<h1 className='title1'>Bienvenido a SIViGILA</h1>
+<p>El Sistema de Salud Pública SIVIGILA tiene como responsabilidad el 
+  proceso de observación y análisis objetivo, sistemático y constante de los eventos en salud, el cual sustenta la orientación, planificación, ejecución, seguimiento y evaluación de la práctica de la salud pública.</p>
+</div>
+ <div >
+
+ </div>
+ <div>
+  </div>
+ </div>
+    
+  )
+}
+
+export default App
